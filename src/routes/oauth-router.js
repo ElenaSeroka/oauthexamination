b9d2ea0f45c9f9674ceebd1293069067d8e69df9
dashdotbunny <70527748+ElenaSeroka@users.noreply.github.com>
2022-04-01 15:14:48 +0200

    Delete src directory

--- src/routes/oauth-router.js
+++
@@ -1,9 +0,0 @@
-
-import express from 'express'
-import { OauthController } from '../controllers/oauth-controller.js'
-
-export const oauthRouter = express.Router()
-export const controller = new OauthController()
-
-oauthRouter.get('/gitlablogin', controller.gitlabLogin)
-oauthRouter.get('/gitlabcallback', controller.handleGitlabCallback)
--- src/routes/page-router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { PageController } from '../controllers/page-controller.js'
-
-export const pageRouter = express.Router()
-export const controller = new PageController()
-
-pageRouter.get('/', controller.renderMain)
-pageRouter.get('/login', controller.renderLoginPage)
-pageRouter.get('/logout', controller.renderLogout, controller.logout)
